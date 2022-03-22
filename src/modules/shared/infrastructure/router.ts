ec5a00d642d6c0225ca4cbf17b4ccf45b27560ff
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-03-22 12:55:37 -0300

    feat: introduce router file

---
+++ src/modules/shared/infrastructure/router.ts
@@ -0,0 +1,8 @@
+import express, { Router } from 'express';
+import userRoutes from '../../user/infrastructure/http/routes';
+
+const router: Router = express.Router();
+
+router.use('/user', userRoutes);
+
+export default router;
