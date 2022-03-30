2ff3e5c53c0c811afef94475d806a68a7e973d14
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-03-30 18:14:38 -0300

    feat(sign-up.service): now actually adds a user to the db

--- src/modules/user/services/sign-up/sign-up.service.ts
+++ src/modules/user/services/sign-up/sign-up.service.ts
@@ -1,10 +1,10 @@
 import Result from '../../../shared/domain/result';
 import Email from '../../domain/email';
-import InvalidEmailError from '../../domain/errors/invalid-email';
 import Password from '../../domain/password';
 import UserRepository from '../../repository/user.repository';
 import SignUpDTO from '../../controllers/sign-up/sign-up.DTO';
 import Username from '../../domain/username';
+import { EmailAlreadyUsedError } from '../../domain/errors';
 
 type Response = Promise<Result<unknown> | Result<Email> | Result<void>>;
 
@@ -28,11 +28,17 @@ export default class SignUp {
       return Result.fail(combinedProperties.error);
     }
 
-    const { email } = emailOrError.value.properties;
+    const { email } = (emailOrError.value as Email).properties;
+
     const emailAlreadyUsed = await this._userRepository.emailAlreadyUsed(email);
     if (emailAlreadyUsed) {
-      return Result.fail<Email>(new InvalidEmailError('E-mail already being used').message);
+      return Result.fail<EmailAlreadyUsedError>(new EmailAlreadyUsedError(email));
     }
+
+    const { username } = (usernameOrError.value as Username).properties;
+    const { hash } = (passwordOrError.value as Password).properties;
+
+    this._userRepository.create({ email, hash, username });
 
     return Result.ok<void>();
   }
