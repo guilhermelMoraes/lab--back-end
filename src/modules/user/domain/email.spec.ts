d2b8a01dd5821f4ba80c1e7d2fe0b159bd320450
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-03-30 17:23:58 -0300

    test(value-objects): add unit test for email and password

---
+++ src/modules/user/domain/email.spec.ts
@@ -0,0 +1,19 @@
+import Result from '../../shared/domain/result';
+import Email from './email';
+import { NonStandardEmailError } from './errors';
+
+describe('Value object: e-mail', () => {
+  it('"john.doe.com" should return an error result', () => {
+    const invalidEmail = 'john.doe.com';
+    const result = Email.create(invalidEmail);
+    expect(result).toEqual(
+      Result.fail<NonStandardEmailError>(new NonStandardEmailError(invalidEmail)),
+    );
+  });
+
+  it('"john.doe@mail.com" should return an successful result', () => {
+    const validEmail = 'john.doe@mail.com';
+    const result = Email.create(validEmail);
+    expect(result.value).toBeInstanceOf(Email);
+  });
+});
---
+++ src/modules/user/domain/password.spec.ts
@@ -0,0 +1,44 @@
+import Result from '../../shared/domain/result';
+import { PasswordLengthError, PasswordMatchConfirmationError } from './errors';
+import Password from './password';
+
+jest.mock('bcrypt', () => ({
+  hash: () => 'hashed-password',
+}));
+
+describe('Value object: password', () => {
+  it('"bad-pas" password should return an error if confirmation is different', async () => {
+    const invalidPass = {
+      password: 'password',
+      passwordConfirmation: 'password-confirmation',
+    };
+
+    const sut = await Password.create(invalidPass);
+    expect(sut).toEqual(Result.fail<PasswordMatchConfirmationError>(
+      new PasswordMatchConfirmationError(invalidPass.password, invalidPass.passwordConfirmation),
+    ));
+  });
+
+  it('should return an error if the password length is less than 8 chars', async () => {
+    const invalidPass = {
+      password: '0123456',
+      passwordConfirmation: '0123456',
+    };
+
+    const sut = await Password.create(invalidPass);
+    expect(sut).toEqual(Result.fail<PasswordLengthError>(
+      new PasswordLengthError(invalidPass.password.length),
+    ));
+  });
+
+  it('should return a successful result', async () => {
+    const validPassword = {
+      password: 'goodPassword_202#!',
+      passwordConfirmation: 'goodPassword_202#!',
+    };
+
+    const sut = await Password.create(validPassword);
+    const { hash } = (sut.value as Password).properties;
+    expect(hash).toBe('hashed-password');
+  });
+});
