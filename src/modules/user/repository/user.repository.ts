b291a9360913a427b6776c13fc799b9961c1f7a1
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-01 09:33:53 -0300

    feat: introduce new routine to user repository

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,5 +1,8 @@
+import User from '../domain/user';
+
 interface UserRepository {
   emailAlreadyUsed(email: string): Promise<boolean>;
+  create(user: User): Promise<void>;
 }
 
 export default UserRepository;
