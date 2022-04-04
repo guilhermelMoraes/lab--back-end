f64708a69c304ea11d64a8cc4643c4d8adbb2001
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-04 14:51:15 -0300

    feat: domain user now generates uuid

--- src/modules/user/domain/user.ts
+++ src/modules/user/domain/user.ts
@@ -1,8 +1,42 @@
-type User = {
-  user_id: string;
-  username: string;
+import Crypto from 'crypto';
+
+import Email from './email';
+import Password from './password';
+import Username from './username';
+
+export type UserProperties = {
+  userId: string;
   email: string;
+  username: string;
   hash: string;
 }
 
-export default User;
+export default class User {
+  private readonly _userId: string;
+  private readonly _email: Email;
+  private readonly _username: Username;
+  private readonly _hash: Password;
+
+  constructor(email: Email, username: Username, password: Password) {
+    this._userId = Crypto.randomUUID();
+    this._email = email;
+    this._username = username;
+    this._hash = password;
+  }
+
+  get userId(): string {
+    return this._userId;
+  }
+
+  get email(): string {
+    return this._email.properties.email;
+  }
+
+  get username(): string {
+    return this._username.properties.username;
+  }
+
+  get hash(): string {
+    return this._hash.properties.hash;
+  }
+}
