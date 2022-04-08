0c07b9ae5f628688d01c6fef18e8cfe7f47f31ee
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-08 16:04:33 -0300

    refactor: server boot is now a facade

--- src/modules/shared/infrastructure/application.ts
+++ src/modules/shared/infrastructure/application.ts
@@ -8,24 +8,45 @@ import express, { Application } from 'express';
 import router from './router';
 import postgresDataSource from './database/config';
 
-const application: Application = express();
-const HTTP_SERVER_PORT: string | number = process.env.HTTP_SERVER_PORT ?? 8000;
+class ServerFacade {
+  private static _databaseConnectionOpen: boolean;
 
-postgresDataSource
-  .initialize()
-  .then(() => {
-    // TODO: implement logging strategy
-    console.log('[Database] Database up and running');
-  })
-  .catch((error) => {
-    // TODO: implement logging strategy
-    console.error(error);
-  });
+  private static async connectToDatabase(): Promise<void> {
+    try {
+      const dbConnection = await postgresDataSource.initialize();
+      this._databaseConnectionOpen = dbConnection.isInitialized;
 
-application.use(express.json());
-application.use('/', router);
+      // TODO: implement logging strategy
+      console.log('[Database] Database up and running');
+    } catch (error) {
+      // TODO: implement logging strategy
+      console.error(error);
+      this._databaseConnectionOpen = false;
+    }
+  }
 
-application.listen(HTTP_SERVER_PORT, (): void => {
-  // TODO: implement logging strategy
-  console.log(`[Server] Server is running on port ${HTTP_SERVER_PORT}`);
-});
+  private static async setUpExpress(): Promise<void> {
+    const application: Application = express();
+    const HTTP_SERVER_PORT: string | number = process.env.HTTP_SERVER_PORT ?? 8000;
+
+    application.use(express.json());
+    application.use('/', router);
+
+    application.listen(HTTP_SERVER_PORT, (): void => {
+      // TODO: implement logging strategy
+      console.log(`[Server] Server is running on port ${HTTP_SERVER_PORT}`);
+    });
+  }
+
+  public static async boot(): Promise<void> {
+    await this.connectToDatabase();
+
+    if (this._databaseConnectionOpen) {
+      this.setUpExpress();
+    }
+  }
+}
+
+(async (): Promise<void> => {
+  await ServerFacade.boot();
+})();
