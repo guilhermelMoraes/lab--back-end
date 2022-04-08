15453b2ba0f00d62429afe6cbb094698a281cbe3
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-08 16:03:09 -0300

    chore: adding more linter rules

--- src/modules/user/domain/user.ts
+++ src/modules/user/domain/user.ts
@@ -24,19 +24,19 @@ export default class User {
     this._hash = password;
   }
 
-  get userId(): string {
+  public get userId(): string {
     return this._userId;
   }
 
-  get email(): string {
+  public get email(): string {
     return this._email.properties.email;
   }
 
-  get username(): string {
+  public get username(): string {
     return this._username.properties.username;
   }
 
-  get hash(): string {
+  public get hash(): string {
     return this._hash.properties.hash;
   }
 }
