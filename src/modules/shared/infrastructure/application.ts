ac0e040bc5857c7fc719684ebf2e6b4f2fcba1bf
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-08 22:26:25 -0300

    refactor: update server facade to inject logger

--- src/modules/shared/infrastructure/application.ts
+++ src/modules/shared/infrastructure/application.ts
@@ -7,25 +7,30 @@ import 'reflect-metadata';
 import express, { Application } from 'express';
 import router from './router';
 import postgresDataSource from './database/config';
+import Logger from '../../../log/logger';
+import WinstonLogger from '../../../log/winston/winston-logger';
 
 class ServerFacade {
-  private static _databaseConnectionOpen: boolean;
+  private readonly _logger: Logger;
+  private _databaseConnectionOpen!: boolean;
 
-  private static async connectToDatabase(): Promise<void> {
+  constructor(logger: Logger) {
+    this._logger = logger;
+  }
+
+  private async connectToDatabase(): Promise<void> {
     try {
       const dbConnection = await postgresDataSource.initialize();
       this._databaseConnectionOpen = dbConnection.isInitialized;
 
-      // TODO: implement logging strategy
-      console.log('[Database] Database up and running');
+      this._logger.info('Database up and running');
     } catch (error) {
-      // TODO: implement logging strategy
-      console.error(error);
+      this._logger.error((error as Error).message);
       this._databaseConnectionOpen = false;
     }
   }
 
-  private static async setUpExpress(): Promise<void> {
+  private async setUpExpress(): Promise<void> {
     const application: Application = express();
     const HTTP_SERVER_PORT: string | number = process.env.HTTP_SERVER_PORT ?? 8000;
 
@@ -33,12 +38,11 @@ class ServerFacade {
     application.use('/', router);
 
     application.listen(HTTP_SERVER_PORT, (): void => {
-      // TODO: implement logging strategy
-      console.log(`[Server] Server is running on port ${HTTP_SERVER_PORT}`);
+      this._logger.info(`Server is running on port ${HTTP_SERVER_PORT}`);
     });
   }
 
-  public static async boot(): Promise<void> {
+  public async boot(): Promise<void> {
     await this.connectToDatabase();
 
     if (this._databaseConnectionOpen) {
@@ -48,5 +52,8 @@ class ServerFacade {
 }
 
 (async (): Promise<void> => {
-  await ServerFacade.boot();
+  const winstonLogger = WinstonLogger.getInstance();
+
+  const application = new ServerFacade(winstonLogger);
+  await application.boot();
 })();
