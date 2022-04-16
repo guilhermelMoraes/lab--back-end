04309e2c960dd9277b6ef6f68a5eb7c831eeb691
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-16 14:06:30 -0300

    refactor: update error message to be more generic

---
+++ src/modules/user/services/login/errors/index.ts
@@ -0,0 +1,1 @@
+export * from './user-or-password-wrong-error';
--- src/modules/user/services/login/errors/user-doesnt-exist-error.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export class UserDoesntExistError extends Error {
-  constructor(userEmail: string) {
-    super(`There are no users associated with ${userEmail}`);
-    this.name = 'UserDoesntExistError';
-  }
-}
