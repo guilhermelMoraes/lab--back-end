e9c82952f830f83889c7ae037937be941f12b985
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-24 15:52:29 -0300

    test(login): add new test suit

--- src/modules/user/services/login/login.spec.ts
+++ src/modules/user/services/login/login.spec.ts
@@ -4,14 +4,35 @@ import UserRepository from '../../repository/user.repository';
 import Result from '../../../shared/domain/result';
 import { NonStandardEmailError } from '../../domain/errors';
 import { UserOrPasswordWrongError } from './errors';
+import JwtClient from '../../../../gateways/jwt';
+import Password from '../../domain/password';
+
+jest.mock('../../domain/password', () => ({
+  compare: () => true,
+}));
+
+class JwtClientStub implements JwtClient {
+  // eslint-disable-next-line class-methods-use-this
+  public async sign<T>(payload: T): Promise<Result<string | Error>> {
+    return Promise.resolve(Result.ok<string>('valid_token'));
+  }
+}
+
+const validUserDummy = {
+  email: 'john.doe@mail.com',
+  password: 'valid-password',
+};
 
 describe('Service: login', () => {
   let sut: Login;
   let userRepository: UserRepository;
+  let jwtClientStub: JwtClient;
 
   beforeAll(() => {
     userRepository = new UserInMemoryRepository();
-    sut = new Login(userRepository);
+    jwtClientStub = new JwtClientStub();
+
+    sut = new Login(userRepository, jwtClientStub);
   });
 
   it('Should return an error if the e-mail is not valid', async () => {
@@ -27,22 +48,14 @@ describe('Service: login', () => {
   });
 
   it('Should return an error if trying to login with non existing user', async () => {
-    const nonExistingUserDummy = {
-      email: 'john.doe@mail.com',
-      password: 'valid-password',
-    };
-
-    const response = await sut.execute(nonExistingUserDummy);
+    const response = await sut.execute(validUserDummy);
     expect(response).toEqual(
       Result.fail<UserOrPasswordWrongError>(new UserOrPasswordWrongError()),
     );
   });
 
   it('Should return an error if password provided doesn\'t match the hash', async () => {
-    const validUserDummy = {
-      email: 'john.doe@mail.com',
-      password: 'valid-password',
-    };
+    jest.spyOn(Password, 'compare').mockResolvedValueOnce(Result.ok<boolean>(false));
 
     jest.spyOn(userRepository, 'findUserByEmail').mockResolvedValueOnce({
       email: 'valid_email',
@@ -53,5 +66,17 @@ describe('Service: login', () => {
 
     const response = await sut.execute(validUserDummy);
     expect(response).toEqual(Result.fail<UserOrPasswordWrongError>(new UserOrPasswordWrongError()));
+  });
+
+  it('Should return a token if the password matches', async () => {
+    jest.spyOn(userRepository, 'findUserByEmail').mockResolvedValueOnce({
+      email: 'valid_email',
+      userId: 'valid_id',
+      hash: 'valid_hash',
+      username: 'valid_username',
+    });
+
+    const response = await sut.execute(validUserDummy);
+    expect(response).toEqual(Result.ok<string>('valid_token'));
   });
 });
