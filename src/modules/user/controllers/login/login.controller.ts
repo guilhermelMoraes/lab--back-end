9cf70227b31577171b761956b397b075a9ab8a61
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-27 08:28:08 -0300

    feat(login): add new controller

---
+++ src/modules/user/controllers/login/login.controller.ts
@@ -0,0 +1,46 @@
+import Controller, { Request, validatePayload } from '../../../shared/http/controller';
+import MissingRequiredParameterError from '../../../shared/http/errors/missing-required-parameter';
+import Login from '../../services/login/login';
+import LoginDTO from '../../services/login/login.DTO';
+
+export default class LoginController extends Controller<LoginDTO> {
+  private readonly _loginService: Login;
+
+  constructor(loginService: Login) {
+    super();
+    this._loginService = loginService;
+  }
+
+  public async handle<T extends LoginDTO>(request: Request<T>): Promise<any> {
+    const payloadValidation = validatePayload<LoginDTO>(
+      request.payload as LoginDTO,
+      ['email', 'password'],
+    );
+
+    if (!payloadValidation.succeeds) {
+      const { message } = new MissingRequiredParameterError(payloadValidation.parameter as string);
+
+      return LoginController.badRequest<string>(message);
+    }
+
+    const login = await this._loginService.execute(request.payload as LoginDTO);
+
+    if (login.isFailure) {
+      const { name, message } = login.error as Error;
+
+      switch (name) {
+        case 'UserDoesntExistError':
+          return LoginController.notFound(message);
+        case 'UserOrPasswordWrongError':
+          return LoginController.unauthorized(message);
+        case 'NonStandardEmailError':
+          return LoginController.badRequest(message);
+        default: {
+          return LoginController.internalServerError();
+        }
+      }
+    }
+
+    return LoginController.ok<string>(login.value as string);
+  }
+}
