d01d3ab546db0e8894e72e95543547987bc5f8dd
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-27 08:26:56 -0300

    fix(controller): remove useless type

--- src/modules/shared/http/controller.ts
+++ src/modules/shared/http/controller.ts
@@ -49,7 +49,7 @@ export default abstract class Controller<DTO> {
     };
   }
 
-  protected static internalServerError(message: string | undefined = 'Internal server error. Please, try again later'): Response<string> {
+  protected static internalServerError(message = 'Internal server error. Please, try again later'): Response<string> {
     return {
       statusCode: 500,
       payload: message,
