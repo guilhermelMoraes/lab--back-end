aa02ff177b7bee8a4c736be86ff8efeda50f1fd7
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-30 21:21:46 -0300

    refactor: split application and server declaration

--- src/modules/shared/infrastructure/application.ts
+++ src/modules/shared/infrastructure/application.ts
@@ -1,52 +1,15 @@
-/* eslint-disable import/first */
-import { config } from 'dotenv';
-
-config();
-
-import 'reflect-metadata';
 import express, { Application } from 'express';
 import router from './router';
-import postgresDataSource from './database/config';
 
-class ServerFacade {
-  private static _databaseConnectionOpen: boolean;
+const application: Application = express();
+const HTTP_SERVER_PORT: string | number = process.env.HTTP_SERVER_PORT ?? 8000;
 
-  private static async connectToDatabase(): Promise<void> {
-    try {
-      const dbConnection = await postgresDataSource.initialize();
-      this._databaseConnectionOpen = dbConnection.isInitialized;
+application.use(express.json());
+application.use('/', router);
 
-      // TODO: implement logging strategy
-      console.log('[Database] Database up and running');
-    } catch (error) {
-      // TODO: implement logging strategy
-      console.error(error);
-      this._databaseConnectionOpen = false;
-    }
-  }
+application.listen(HTTP_SERVER_PORT, (): void => {
+  // TODO: implement logging strategy
+  console.log(`[Server] Server is running on port ${HTTP_SERVER_PORT}`);
+});
 
-  private static async setUpExpress(): Promise<void> {
-    const application: Application = express();
-    const HTTP_SERVER_PORT: string | number = process.env.HTTP_SERVER_PORT ?? 8000;
-
-    application.use(express.json());
-    application.use('/', router);
-
-    application.listen(HTTP_SERVER_PORT, (): void => {
-      // TODO: implement logging strategy
-      console.log(`[Server] Server is running on port ${HTTP_SERVER_PORT}`);
-    });
-  }
-
-  public static async boot(): Promise<void> {
-    await this.connectToDatabase();
-
-    if (this._databaseConnectionOpen) {
-      this.setUpExpress();
-    }
-  }
-}
-
-(async (): Promise<void> => {
-  await ServerFacade.boot();
-})();
+export default application;
---
+++ src/modules/shared/infrastructure/server.ts
@@ -0,0 +1,37 @@
+/* eslint-disable import/first */
+import { config } from 'dotenv';
+import http from 'http';
+
+config();
+
+import 'reflect-metadata';
+import application from './application';
+import postgresDataSource from './database/config';
+
+let _databaseConnectionOpen = false;
+
+async function connectToDatabase(): Promise<void> {
+  try {
+    const dbConnection = await postgresDataSource.initialize();
+    _databaseConnectionOpen = dbConnection.isInitialized;
+
+    // TODO: implement logging strategy
+    console.log('[Database] Database up and running');
+  } catch (error) {
+    // TODO: implement logging strategy
+    console.error(error);
+    _databaseConnectionOpen = false;
+  }
+}
+
+async function bootServer(): Promise<void> {
+  await connectToDatabase();
+
+  if (_databaseConnectionOpen) {
+    http.createServer(application);
+  }
+}
+
+(async (): Promise<void> => {
+  await bootServer();
+})();
