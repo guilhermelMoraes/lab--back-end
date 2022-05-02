ac46028b0901223073782044bc8ff15d60746acf
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-05-02 15:26:54 -0300

    chore: setup to use path aliases

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
@@ -5,4 +9,6 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  moduleDirectories: ['.', 'src', 'node_modules'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
