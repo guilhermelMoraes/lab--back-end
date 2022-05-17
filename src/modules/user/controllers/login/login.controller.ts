e18f12e86700f197c8258dfc1a6550433e564a3a
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-05-17 13:47:12 -0300

    fix: add try/catch block to login controller

--- src/modules/user/controllers/login/login.controller.ts
+++ src/modules/user/controllers/login/login.controller.ts
@@ -13,25 +13,30 @@ export default class LoginController extends Controller<LoginDTO> {
   }
 
   public async handle<T extends LoginDTO>(request: Request<T>): Promise<any> {
-    const login = await this._loginService.execute(request.payload as LoginDTO);
+    try {
+      const login = await this._loginService.execute(request.payload as LoginDTO);
 
-    if (login.isFailure) {
-      const { name, message } = login.error as Error;
+      if (login.isFailure) {
+        const { name, message } = login.error as Error;
 
-      switch (name) {
-        case 'UserDoesntExistError':
-          return LoginController.notFound(message);
-        case 'UserOrPasswordWrongError':
-          return LoginController.unauthorized(message);
-        case 'NonStandardEmailError':
-        case 'TypeError':
-          return LoginController.badRequest<string>(message);
-        default: {
-          return LoginController.internalServerError();
+        switch (name) {
+          case 'UserDoesntExistError':
+            return LoginController.notFound(message);
+          case 'UserOrPasswordWrongError':
+            return LoginController.unauthorized(message);
+          case 'NonStandardEmailError':
+          case 'TypeError':
+            return LoginController.badRequest<string>(message);
+          default: {
+            // TODO: implement logging strategy
+            return LoginController.internalServerError();
+          }
         }
       }
+      return LoginController.ok<string>(login.value as string);
+    } catch (error) {
+      // TODO: implement logging strategy
+      return LoginController.internalServerError();
     }
-
-    return LoginController.ok<string>(login.value as string);
   }
 }
