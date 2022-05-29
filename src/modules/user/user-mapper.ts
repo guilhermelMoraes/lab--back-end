097cf9869af2beac4803fefa6a9299c208475f02
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-05-28 22:31:05 -0300

    refactor(user-mapper): update  method
    to reflect new signature for value objects

--- src/modules/user/user-mapper.ts
+++ src/modules/user/user-mapper.ts
@@ -24,8 +24,8 @@ export default class UserMapper {
 
   public static toDomain(user: UserProperties): User {
     const { userId, isEmailVerified, hash } = user;
-    const email = Email.create(user.email).value as Email;
-    const username = Username.create(user.username).value as Username;
+    const email = Email.create(user.email) as Email;
+    const username = Username.create(user.username) as Username;
     const password = new Password({ hash });
 
     return new User(email, username, password, userId, isEmailVerified);
